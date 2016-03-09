cf3ff50f5a91fc3dd88e9eaeb0b9c1335ed60dd8
John0079 <270752265@qq.com>
2016-03-09 20:50:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/util/MyTestUtil.java
@@ -0,0 +1,10 @@
+package com.example.myapplication.util;
+
+/**
+ * Created by 金鹏 on 2016/3/9.
+ */
+public class MyTestUtil {
+    public String getValue() {
+        return "hello form thes";
+    }
+}
